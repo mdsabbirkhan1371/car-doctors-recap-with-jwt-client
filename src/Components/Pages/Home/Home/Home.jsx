1013657c5baa3dd9623490d4488b0865caa74c95
Md Sabbir Khan <mdsabbirkhan1972@gmail.com>
2024-05-30 13:28:34 +0600

    first

---
+++ src/Components/Pages/Home/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Banner from '../Banner/Banner';
+
+const Home = () => {
+  return (
+    <div>
+      <Banner></Banner>
+      <h3>This is Home</h3>
+    </div>
+  );
+};
+
+export default Home;
