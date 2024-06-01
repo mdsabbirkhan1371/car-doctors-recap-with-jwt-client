387aab54f51c9ea6282b5812647f97ef3249d261
Md Sabbir Khan <mdsabbirkhan1972@gmail.com>
2024-06-02 00:44:32 +0600

    deleted booking

--- src/Components/Pages/Bookings/Bookings.jsx
+++ src/Components/Pages/Bookings/Bookings.jsx
@@ -1,11 +1,13 @@
 import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../Providers/AuthProvider';
 import BookingsRow from './BookingsRow';
+import { ToastContainer, toast } from 'react-toastify';
 
 const Bookings = () => {
   const { user } = useContext(AuthContext);
 
   const [myBookings, setMyBookings] = useState([]);
+
   const url = `http://localhost:5000/bookings?email=${user.email}`;
   useEffect(() => {
     fetch(url)
@@ -15,6 +17,24 @@ const Bookings = () => {
         setMyBookings(data);
       });
   }, []);
+
+  const handleDelete = id => {
+    const proceed = confirm('Are You Sure Want to Delete This?');
+    if (proceed) {
+      fetch(`http://localhost:5000/bookings/${id}`, {
+        method: 'DELETE',
+      })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+          if (data.deletedCount > 0) {
+            toast('Deleted Successful');
+            const remaining = myBookings.filter(booking => booking._id !== id);
+            setMyBookings(remaining);
+          }
+        });
+    }
+  };
   return (
     <div>
       <h1 className="text-center text-2xl">
@@ -26,23 +46,26 @@ const Bookings = () => {
           <thead>
             <tr>
               <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
+                <label>Delete</label>
               </th>
               <th>Image</th>
               <th>Service Name</th>
               <th>Email</th>
               <th>Price</th>
-              <th></th>
+              <th>Status</th>
             </tr>
           </thead>
           <tbody>
             {myBookings.map(booking => (
-              <BookingsRow key={booking._id} booking={booking}></BookingsRow>
+              <BookingsRow
+                handleDelete={handleDelete}
+                key={booking._id}
+                booking={booking}
+              ></BookingsRow>
             ))}
           </tbody>
         </table>
+        <ToastContainer></ToastContainer>
       </div>
     </div>
   );
--- src/Components/Pages/Bookings/BookingsRow.jsx
+++ src/Components/Pages/Bookings/BookingsRow.jsx
@@ -1,11 +1,26 @@
-const BookingsRow = ({ booking }) => {
-  console.log(booking);
-  const { img, service, price, email } = booking;
+import PropTypes from 'prop-types';
+const BookingsRow = ({ booking, handleDelete }) => {
+  const { _id, img, service, price, email } = booking;
   return (
     <tr>
       <th>
         <label>
-          <input type="checkbox" className="checkbox" />
+          <button onClick={() => handleDelete(_id)} className="btn btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
         </label>
       </th>
       <td>
@@ -29,3 +44,8 @@ const BookingsRow = ({ booking }) => {
 };
 
 export default BookingsRow;
+
+BookingsRow.propTypes = {
+  booking: PropTypes.object,
+  handleDelete: PropTypes.func,
+};
